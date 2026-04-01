e2c0ecc2655747382b8156705cdc7a281283dcc2
NikVinogradoff <vinogradovn481@gmail.com>
2026-04-01 18:56:20 +0300

    users full

--- resources/users_resources.py
+++ resources/users_resources.py
@@ -12,6 +12,19 @@ def abort_if_users_not_found(users_id):
         abort(404, message=f"News {users_id} not found")
 
 
+parser = reqparse.RequestParser()
+parser.add_argument("surname", required=True)
+parser.add_argument("name", required=True)
+parser.add_argument("age", required=True, type=int)
+parser.add_argument("collaborators", required=True)
+parser.add_argument("position", required=True)
+parser.add_argument("speciality", required=True)
+parser.add_argument("address", required=True)
+parser.add_argument("email", required=True)
+parser.add_argument("hashed_password", required=True)
+parser.add_argument("modified_date", required=True)
+
+
 class UsersResource(Resource):
     def get(self, users_id):
         abort_if_users_not_found(users_id)
@@ -30,18 +43,29 @@ class UsersResource(Resource):
         session.commit()
         return jsonify({"success": "ok"})
 
-
-parser = reqparse.RequestParser()
-parser.add_argument("surname", required=True)
-parser.add_argument("name", required=True)
-parser.add_argument("age", required=True, type=int)
-parser.add_argument("collaborators", required=True)
-parser.add_argument("position", required=True)
-parser.add_argument("speciality", required=True)
-parser.add_argument("address", required=True)
-parser.add_argument("email", required=True)
-parser.add_argument("hashed_password", required=True)
-parser.add_argument("modified_date", required=True)
+    def put(self, users_id):
+        abort_if_users_not_found(users_id)
+        args = parser.parse_args()
+        session = db_session.create_session()
+        users = User(
+            id=users_id,
+            surname=args["surname"],
+            name=args["name"],
+            age=args["age"],
+            position=args["position"],
+            speciality=args["speciality"],
+            address=args["address"],
+            email=args["email"],
+            hashed_password=args["hashed_password"],
+            modified_date=args["modified_date"],
+            is_finished=args["is_finished"]
+        )
+        session.merge(users)
+        session.commit()
+        return jsonify(users.to_dict(only=(
+            "id", "surname", "name", "age", "position", "speciality", "address",
+            "email", "modified_date"
+        )))
 
 
 class UsersListResource(Resource):
@@ -49,7 +73,7 @@ class UsersListResource(Resource):
         session = db_session.create_session()
         users = session.query(User).all()
         return jsonify({
-            "jobs": [
+            "users": [
                 user.to_dict(only=(
                     "id", "surname", "name", "age", "position", "speciality", "address",
                     "email", "hashed_password", "modified_date")
